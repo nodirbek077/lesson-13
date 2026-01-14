cfe7697686fb710930da0ff937d26978ae789be9
Nodirbek <nodirbeknurqulov096@gmail.com>
2026-01-14 17:01:16 +0500

    add and check contact methods have been implemented

---
+++ src/Contact.java
@@ -0,0 +1,5 @@
+public class Contact {
+    String name;
+    String surname;
+    String phone;
+}
--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,11 @@ public class Main {
 
             switch (n) {
                 case 1:
-                    //
+                    Contact contact = addContact();
+                    if (!isValidContact(contact)) {
+                        //
+                    }
+                    System.out.println(contact.name + " " + contact.surname);
                     break;
                 case 2:
                     //
@@ -28,6 +32,57 @@ public class Main {
             }
         }
         System.out.println("The End");
+    }
+
+    public static Contact addContact() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = scanner.next();
+
+        System.out.print("Enter surname: ");
+        String surname = scanner.next();
+
+        System.out.print("Enter phone: ");
+        String phone = scanner.next();
+
+        Contact contact = new Contact();
+        contact.name = name;
+        contact.surname = surname;
+        contact.phone = phone;
+
+        return contact;
+    }
+
+    public static boolean isValidContact(Contact contact) {
+        if (contact.name == null || contact.name.trim().length() < 3) {
+            System.out.println("Contact name is wrong!");
+            return false;
+        }
+
+        if (contact.surname == null || contact.surname.trim().length() < 3) {
+            System.out.println("Contact surname is wrong!");
+            return false;
+        }
+
+        //998945283534 => length = 12
+        if (contact.phone == null || contact.phone.trim().length() != 12 ||
+                !contact.phone.startsWith("998")) {
+            System.out.println("Contact phone is wrong!");
+            return false;
+        }
+
+        //check phone for digits not alphabet
+        char[] phoneArr = contact.phone.toCharArray();//[9,9,8,9,4,5,2,8,3,5,3,4]
+        for (char c : phoneArr) {
+            if (!Character.isDigit(c)) {
+                System.out.println("Contact phone is wrong!");
+                return false;
+            }
+        }//c < '0' || c > '9'
+
+        return true;
+
     }
 
     public static void menu() {
